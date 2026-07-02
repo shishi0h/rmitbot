2fe99f0917743a28bef41fad8f8d434793db444f
shishi0h <bachdam.rain@gmail.com>
2026-07-02 09:01:48 +0700

    pin changes

--- src/rmitbot_firmware/arduino_firmware/rmitbot_arduino_firmware/include/MyIMU.h
+++ src/rmitbot_firmware/arduino_firmware/rmitbot_arduino_firmware/include/MyIMU.h
@@ -4,8 +4,8 @@
 #include <Arduino.h> // Arduino library for basic functions
 #include "ICM_20948.h"
 
-#define I2C_SDA 23 // Pin used on ESP32 for I2C SDA
-#define I2C_SCL 22 // Pin used on ESP32 for I2C SCL
+#define I2C_SDA 22 // Pin used on ESP32 for I2C SDA
+#define I2C_SCL 21 // Pin used on ESP32 for I2C SCL
 
 void IMUBegin();     // Function to initialize the IMU
 void IMUGetData();   // Function to get the data from the IMU
