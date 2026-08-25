6c82b35d086e3bfbc3d395212f471811b8f32eed
Loc <locnguyenthanh2411@gmail.com>
2026-08-25 15:09:10 +0700

    test track

--- src/rmitbot_bringup/launch/rpi.launch.py
+++ src/rmitbot_bringup/launch/rpi.launch.py
@@ -113,10 +113,10 @@ def generate_launch_description():
     
     return LaunchDescription([
         rsp, 
-        #controller,
+        controller,
         localization,
-        rplidar, 
-        slamtoolbox, 
+        # rplidar, 
+        # slamtoolbox, 
         #cliff_sensor,
     ])
     
